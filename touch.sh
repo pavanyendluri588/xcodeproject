46048b03512b6f0ad4b662c58f0404f048a07b55
pavan <pavan@pavans-iMac.local>
2023-10-25 10:55:07 +0530

    25-10-23 upload

--- touch.sh
+++ touch.sh
@@ -1,5 +1,5 @@
 git init
 git add .
-git add origin "https://github.com/pavanyendluri588/xcodeproject.git"
+git remote add origin "https://github.com/pavanyendluri588/xcodeproject.git"
 git commit -m "25-10-23 upload"
 Git push origin master
