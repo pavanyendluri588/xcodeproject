25a110b3e4994db64698a7b5eaf15ace9b629203
pavanyendluri588 <pavanyendluri588@gmail.com>
2023-10-25 11:26:54 +0530

    25-10-23 upload

--- touch.sh
+++ touch.sh
@@ -1,5 +1,6 @@
 git init
 git add .
-git remote add origin "https://github.com/pavanyendluri588/xcodeproject.git"
+git remote add origin "https://git push -u origin main
+@github.com/pavanyendluri588/xcodeproject.git"
 git commit -m "25-10-23 upload"
 Git push origin master
